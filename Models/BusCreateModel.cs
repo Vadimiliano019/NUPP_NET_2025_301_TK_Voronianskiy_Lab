cb0c37a565684cbe3b48922f5210766558e57e5a
Vadimiliano019 <vadimvoronianskiy@gmail.com>
2025-05-17 16:18:24 +0300

    lab4 - done

---
+++ Models/BusCreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BusStation.REST.Models
+{
+    public class BusCreateModel
+    {
+        public string NumberPlate { get; set; }
+        public string Route { get; set; }
+        public int Capacity { get; set; }
+    }
+}
---
+++ Models/BusModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusStation.REST.Models
+{
+    public class BusModel
+    {
+        public Guid Id { get; set; }
+        public string NumberPlate { get; set; }
+        public string Route { get; set; }
+        public int Capacity { get; set; }
+    }
+}
